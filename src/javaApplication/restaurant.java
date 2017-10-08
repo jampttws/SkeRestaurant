e9322c1872c2c677bf42a30d3c479d2bca2dd105
jampttws <tanasorn42@hotmail.com>
2017-10-08 20:01:25 +0700

    first commit

--- src/javaApplication/restaurant.java
+++ src/javaApplication/restaurant.java
@@ -1,19 +1,18 @@
 package javaApplication;
-//
-//import java.util.Scanner;
-//import java.util.Random;
-//
 
 import java.util.Scanner;
 
 public class restaurant {
-	public static String mc = "";
+
+    static Scanner sc = new Scanner(System.in);
 	
-	public static double total = 0;
+    static String mc = "";
 	
-	public static double change;
+    static double total, totalp, cash, chnge, mem, change;
 	
-        public static void head(int h) {
+    static int price = 0, ch = 0, qty = 0, pz = 0, ck = 0, coke = 0, sp = 0, h = 0, totalq = 0;
+	
+    public static void head(int h) {
 	        System.out.println("============= Welcome to SKE Restaurant ===========");
     	        System.out.println("1.) Pizza                               250 Baht.");
     	        System.out.println("2.) Chickens                            120 Baht.");
@@ -22,11 +21,9 @@ public class restaurant {
     	        System.out.println("5.) Total");
     	        System.out.println("6.) Exit");
 	}
-
-	public static void menu(int pz,int ck,int sp,int coke) {
-		int totalq = 0;
-		double totalp = 0;
-		System.out.println("");
+    
+    public static void hmenu(int pz,int ck,int sp,int coke) {
+    	System.out.println("");
 		System.out.println("   + ========================================== +");
 		System.out.println("   |                SKE Restaurant              |");
 		System.out.println("   + ================   =========   =========== +");
@@ -56,44 +53,22 @@ public class restaurant {
 		    totalp += cok;
 		    totalq += coke; 
 		} 
+    }
+
+	public static void menu(int pz,int ck,int sp,int coke) {
+		hmenu(pz,ck,sp,coke);
 		System.out.println("   + ========================================== +");
 		System.out.printf("   | %2d Items                      %12.2f |\n",totalq,totalp);
 		System.out.println("   + ========================================== +");		
 	}
 	
 	public static void outt(int pz,int ck,int sp,int coke) {
-		int totalq = 0;
-		double totalp = 0, cash = 0, mem = 0;
-		System.out.println("");
-		System.out.println("   + ========================================== +");
-		System.out.println("   |                SKE Restaurant              |");
-		System.out.println("   + ================   =========   =========== +");
-		System.out.println("   |       Menu            Qty         Price    |");	
-		System.out.println("   + ================   =========   =========== +");
-		double pzz = pz*250;
-		double ckk = ck*120;
-		double spp = sp*150;
-		double cok = coke*45;
-		if (pzz > 1) {
-		    System.out.printf("   | Pizza                  %5d  %12.2f |\n",pz,pzz);
-		    totalp += pzz;
-		    totalq += pz;
-		} 
-		if (ckk > 1) {
-			System.out.printf("   | Chickens               %5d  %12.2f |\n",ck,ckk);
-		    totalp += ckk;
-		    totalq += ck;
-		}
-		if (spp > 1) {
-			System.out.printf("   | Spaghetti              %5d  %12.2f |\n",sp,spp);
-		    totalp += spp;
-		    totalq += sp; 
-		}
-		if (cok > 1) {
-			System.out.printf("   | Coke                   %5d  %12.2f |\n",coke,cok);
-		    totalp += cok;
-		    totalq += coke; 
-		} 
+		
+       	System.out.print("Do you have member card?: ");
+       	mc = sc.next();
+       	
+       	hmenu(pz,ck,sp,coke);
+       	
 		System.out.println("   + ========================================== +");
 		System.out.printf("   | %2d Items                      %12.2f |\n",totalq,totalp);
 		double vat = totalp*7/100;
@@ -109,7 +84,8 @@ public class restaurant {
         	System.out.printf("   | Total                         %12.2f |\n",totalp+vat);	
         }
 		
-		System.out.println("   + ========================================== +");				
+		System.out.println("   + ========================================== +");		
+		
 	}
 	
     public static void note(double size,double money) {
@@ -120,6 +96,7 @@ public class restaurant {
         }
         change = money%size;
 }
+    
     public static void coin(double size,double money) {
     	if ((int)(money/size) != 0) {
     		System.out.println((int)size+" coins: " + (int)(money/size));
@@ -129,11 +106,9 @@ public class restaurant {
         change = money%size;
 }
 	
-    public static void main (String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	int price = 0, ch = 0, qty = 0, pz = 0, ck = 0, coke = 0, sp = 0, h = 0;
-    	head(h);
-       	while (true) {
+    public static void loopMenu() {
+    	
+    	while (true) {
     		System.out.println(" ");
        		System.out.print("Enter your Choice: ");
     		ch = sc.nextInt();
@@ -145,23 +120,24 @@ public class restaurant {
     			System.out.print("Enter Quantity: ");
         		qty = sc.nextInt();
         		if (ch == 1) {
-        			pz = pz + qty;
+        			pz += qty;
         		} else if (ch == 2) {        			
-        			ck = ck + qty;
+        			ck += qty;
         		} else if (ch == 3) {
-        			sp = sp + qty;
+        			sp += qty;
         		} else if (ch == 4) {
-        			coke = coke + qty;
+        			coke += qty;
         		}
     		}    		
-    	}    	
-       	System.out.print("Do you have member card?: ");
-       	mc = sc.next();
-       	outt(pz,ck,sp,coke);
+    	}
+    }
+    
+    public static void mchange(double mon) {
        	System.out.println(" ");
        	System.out.print("Input your cash: ");
-       	double cash = sc.nextDouble();
-       	double chnge = Math.floor(cash-total);
+    	cash = sc.nextDouble();
+    	System.out.println(" ");
+       	chnge = Math.floor(cash-total);
         System.out.printf("Change : %.2f\n",chnge);
        	change = chnge;
         note(1000,change);
@@ -173,9 +149,14 @@ public class restaurant {
         coin(5,change);
         coin(2,change);
         coin(1,change);
-        
-       	System.out.println("");
+        System.out.println("");
         System.out.print("    ================= Thank you ================");
+    }
+	
+    public static void main (String[] args) {   	
+    	head(h);
+       	loopMenu();
+       	outt(pz,ck,sp,coke);
+        mchange(cash);
     }	 
-
 }
